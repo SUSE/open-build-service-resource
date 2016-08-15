dcb2652267e9d55010bd91a468fd46e03e03c4c3
Mario Manno <mario.manno@suse.com>
2016-08-15 14:04:54 +0200

    update example

--- examples/build.sh
+++ examples/build.sh
@@ -3,6 +3,18 @@ set -e
 sed -i "s|<username>|$OSC_USERNAME|g" /root/.oscrc
 sed -i "s|<password>|$OSC_PASSWORD|g" /root/.oscrc
 
-osc log --csv
+pushd osc-resource/home:mmanno/restic 1> /dev/null
 
-osc service run
+  if [ -f test ]; then
+    osc rm test
+    osc commit -m 'remove test'
+  fi
+
+  touch test
+  ls -la
+
+  osc log --xml | tee
+
+popd 1> /dev/null
+
+cp -a osc-resource/home:mmanno/restic/. osc-updated/
